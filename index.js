f968584ca4fb6242966aaeeb81576926096143cb
shravan20 <mrshravankumarb@gmail.com>
2020-11-14 01:08:29 +0530

    Feature | basics moves feature done

--- index.js
+++ index.js
@@ -63,10 +63,121 @@ document.addEventListener('DOMContentLoaded', () => {
         
       }
 
+      fall = ()=> {
+        isJumping = false
+          clearInterval(upTimerId)
+          downTimerId = setInterval(function () {
+            doodlerBottomSpace -= 5
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace <= 0) {
+              gameOver()
+            }
+            platforms.forEach(platform => {
+              if (
+                (doodlerBottomSpace >= platform.bottom) &&
+                (doodlerBottomSpace <= (platform.bottom + 15)) &&
+                ((doodlerLeftSpace + 60) >= platform.left) && 
+                (doodlerLeftSpace <= (platform.left + 85)) &&
+                !isJumping
+                ) {
+                  console.log('tick')
+                  startPoint = doodlerBottomSpace
+                  jump()
+                  console.log('start', startPoint)
+                  isJumping = true
+                }
+            })
+      
+          },20)
+      }
+      
+        jump = ()=> {
+          clearInterval(downTimerId)
+          isJumping = true
+          upTimerId = setInterval(function () {
+            console.log(startPoint)
+            console.log('1', doodlerBottomSpace)
+            doodlerBottomSpace += 20
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            console.log('2',doodlerBottomSpace)
+            console.log('s',startPoint)
+            if (doodlerBottomSpace > (startPoint + 200)) {
+              fall()
+              isJumping = false
+            }
+          },30)
+        }
+      
+        moveLeft = () => {
+          if (isGoingRight) {
+              clearInterval(rightTimerId)
+              isGoingRight = false
+          }
+          isGoingLeft = true
+          leftTimerId = setInterval(function () {
+              if (doodlerLeftSpace >= 0) {
+                console.log('going left')
+                doodlerLeftSpace -=5
+                 doodler.style.left = doodlerLeftSpace + 'px'
+              } else moveRight()
+          },20)
+        }
+      
+        moveRight = () => {
+          if (isGoingLeft) {
+              clearInterval(leftTimerId)
+              isGoingLeft = false
+          }
+          isGoingRight = true
+          rightTimerId = setInterval(function () {
+            //changed to 313 to fit doodle image
+            if (doodlerLeftSpace <= 313) {
+              console.log('going right')
+              doodlerLeftSpace +=5
+              doodler.style.left = doodlerLeftSpace + 'px'
+            } else moveLeft()
+          },20)
+        }
+        
+        moveStraight =()=> {
+          isGoingLeft = false
+          isGoingRight = false
+          clearInterval(leftTimerId)
+          clearInterval(rightTimerId)
+        }
+      
+        //assign functions to keyCodes
+        control =(e)=> {
+          doodler.style.bottom = doodlerBottomSpace + 'px'
+          if(e.key === 'ArrowLeft') {
+            moveLeft()
+          } else if (e.key === 'ArrowRight') {
+            moveRight()
+          } else if (e.key === 'ArrowUp') {
+            moveStraight()
+          }
+        }
+      
+        gameOver = () => {
+          isGameOver = true
+          while (grid.firstChild) {
+            console.log('remove')
+            grid.removeChild(grid.firstChild)
+          }
+          grid.innerHTML = score
+          clearInterval(upTimerId)
+          clearInterval(downTimerId)
+          clearInterval(leftTimerId)
+          clearInterval(rightTimerId)
+        }
+
     start = () => {
         if (!isGameOver){
             createDoodler();
             createPlatform();
+            setInterval(movePlatforms,30)
+            jump(startPoint)
+            document.addEventListener('keyup', control)
         }
             
     
